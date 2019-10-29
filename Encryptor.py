e2a6bd238abe656014bfba3e7753250b7e894ac2
BelchiorSaeg <54915187+BelchiorSaeg@users.noreply.github.com>
2019-10-29 11:26:34 -0300

    Atualização utilizando novos métodos

--- Encryptor.py
+++ Encryptor.py
@@ -3,76 +3,105 @@
 @author: Belchior
 
 """
+
 import random
 
-##   Obs: o termo "Célula" é usado neste algoritmo para designar cada valor de uma lista.
+##   Obs: o termo "Célula" é usado neste algoritmo para designar cada valor de uma lista
+##   (podendo ser uma letra da mensagem original, ou seu valor resultante).
 
-### ---- Funções ----
+#################################################################################################################
 
-##   Converte uma lista em String única
-def ListToText(text_list): return ''.join(text_list)
+### *** Funções *** ###
+
+#-----------------------------------------------------------------------------------------------------------------
+# Limpa tela
+
+def clean(): print("\n" * 45)
+    
+#-----------------------------------------------------------------------------------------------------------------
+# Impõem uma decisão ao usuário
+
+def escolha(cls_1, msg, cls_2):
+    
+    if cls_1 == "cls": clean()
+    print(msg)
+    choice = input(">> ")
+    if cls_2 == "cls": clean()
+    
+    return choice
+
+#-----------------------------------------------------------------------------------------------------------------
+# Pede Confirmação ao Usuário
+
+def confirmar(cls_1, msg, cls_2):
+    
+    confirm = "{:.1}".format(escolha(cls_1, msg, cls_2))
+    
+    if   confirm == "s": return True
+    elif confirm == "n": 
+        print("Operação Cancelada! \n\n")
+        return False
+    
+    print("Opção Invalida! \n\n")
+
+#-----------------------------------------------------------------------------------------------------------------
+# Lê arquivo de Texto e retorna o conteúdo
+    
+def ler_arquivo(nome):
+    
+    nome     = nome.replace(".txt", "")
+    arquivo  = open(nome + ".txt", 'r')
+    conteudo = ""
+    
+    conteudo = arquivo.readlines()
+
+    arquivo.close()
+    return conteudo
+
+#-----------------------------------------------------------------------------------------------------------------
+# Escreve um conteúdo em um arquivo de texto
+
+def escrever_arquivo(nome, conteudo):
+    
+    nome    = nome.replace(".txt", "")
+    arquivo = open(nome + ".txt", 'w')
+    
+    arquivo.writelines(conteudo)
+    arquivo.close()
+
+#-----------------------------------------------------------------------------------------------------------------
+# Dicionários contendo os valores que cada celula irá assumir
 
 
-##   Pega a quantidade de celula em cada valor e retorna uma letra correspondente
-def separador(sep): return random.choice(list("ABCDEFGHIJKLMNO"))
+dicio_encript = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14,
+ 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26, 'A': 27,
+ 'B': 28, 'C': 29, 'D': 30, 'E': 31, 'F': 32, 'G': 33, 'H': 34, 'I': 35, 'J': 36, 'K': 37, 'L': 38, 'M': 39, 'N': 40,
+ 'O': 41, 'P': 42, 'Q': 43, 'R': 44, 'S': 45, 'T': 46, 'U': 47, 'V': 48, 'W': 49, 'X': 50, 'Y': 51, 'Z': 52, ' ': 53,
+ '-': 54, '_': 55, '+': 56, '=': 57, ')': 58, '(': 59, '*': 60, '%': 61, '#': 62, '@': 63, '!': 64, "'": 65, '[': 66,
+ ']': 67, '{': 68, '}': 69, 'ã': 70, 'Ã': 71, 'ô': 72, 'Ô': 73, 'é': 74, 'É': 75, 'á': 76, 'Á': 77, 'à': 78, 'À': 79,
+ 'ê': 80, 'Ê': 81, 'Ç': 82, 'ç': 83, '&': 84, 'º': 85, '|': 86, ',': 87, '.': 88, ';': 89, ':': 90, 'ó': 91, 'Ó': 92,
+ '0': 93, '1': 94, '2': 95, '3': 96, '4': 97, '5': 98, '6': 99, '7': 100, '8': 101, '9': 102, '<': 103, '>': 104,
+ '~': 105, '$': 106, '¨': 107, '/': 108, '\\': 109, '"': 110, '?': 111}
 
+dicio_decript = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j', 11: 'k', 12: 'l', 13: 'm', 14: 'n',
+ 15: 'o', 16: 'p', 17: 'q', 18: 'r', 19: 's', 20: 't', 21: 'u', 22: 'v', 23: 'w', 24: 'x', 25: 'y', 26: 'z', 27: 'A',
+ 28: 'B', 29: 'C', 30: 'D', 31: 'E', 32: 'F', 33: 'G', 34: 'H', 35: 'I', 36: 'J', 37: 'K', 38: 'L', 39: 'M', 40: 'N',
+ 41: 'O', 42: 'P', 43: 'Q', 44: 'R', 45: 'S', 46: 'T', 47: 'U', 48: 'V', 49: 'W', 50: 'X', 51: 'Y', 52: 'Z', 53: ' ',
+ 54: '-', 55: '_', 56: '+', 57: '=', 58: ')', 59: '(', 60: '*', 61: '%', 62: '#', 63: '@', 64: '!', 65: "'", 66: '[',
+ 67: ']', 68: '{', 69: '}', 70: 'ã', 71: 'Ã', 72: 'ô', 73: 'Ô', 74: 'é', 75: 'É', 76: 'á', 77: 'Á', 78: 'à', 79: 'À',
+ 80: 'ê', 81: 'Ê', 82: 'Ç', 83: 'ç', 84: '&', 85: 'º', 86: '|', 87: ',', 88: '.', 89: ';', 90: ':', 91: 'ó', 92: 'Ó',
+ 93: '0', 94: '1', 95: '2', 96: '3', 97: '4', 98: '5', 99: '6', 100: '7', 101: '8', 102: '9', 103: '<', 104: '>',
+ 105: '~', 106: '$', 107: '¨', 108: '/', 109: '\\', 110: '"', 111: '?'}
 
-##   Peso que Adiciona Compexibilidade á mensagem criptografada
-def complexidade(qntd_celula): return ((qntd_celula + int(chave_dec[4])) * int(chave_dec[0]) - int(chave_dec[2]))
+#-----------------------------------------------------------------------------------------------------------------
+# Valor que adiciona a complexidade/encriptação à célula
+    
+def complexidade(n_celula): return (n_celula)# + int(chave_dec[4])) * int(chave_dec[0]) - int(chave_dec[2]))
+    
+#-----------------------------------------------------------------------------------------------------------------
+#   gera a chave de encriptação, exibi e a retorna
 
-
-##   Remove a Complexidade
-def descomplexar(qntd_celula): return - ((qntd_celula + int(chave_dec[2])) * int(chave_dec[0]) - int(chave_dec[1]))
-
-
-##   Tabela de Valores - Contém o novo valor para cada caractere a ser criptografado
-def tabela():
-    # |001 = "a" |002 = "b" |003 = "c" |004 = "d" |005 = "e" |006 = "f" |007 = "g" |008 = "h" |009 = "i" |010 = "j" |011 = "k" |012 = "l" |013 = "m" |014 = "n"|
-    # |015 = "o" |016 = "p" |017 = "q" |018 = "r" |019 = "s" |020 = "t" |021 = "u" |022 = "v" |023 = "w" |024 = "x" |025 = "y" |026 = "z" |027 = "A" |028 = "B"|
-    # |029 = "C" |030 = "D" |031 = "E" |032 = "F" |033 = "G" |034 = "H" |035 = "I" |036 = "J" |037 = "K" |038 = "L" |039 = "M" |040 = "N" |041 = "O" |042 = "P"|
-    # |043 = "Q" |044 = "R" |045 = "S" |046 = "T" |047 = "U" |048 = "V" |049 = "W" |050 = "X" |051 = "Y" |052 = "Z" |053 = " " |054 = "-" |055 = "_" |056 = "+"|
-    # |057 = "=" |058 = ")" |059 = "(" |060 = "*" |061 = "%" |062 = "#" |063 = "@" |064 = "!" |065 = "'" |066 = "[" |067 = "]" |068 = "{" |069 = "}" |070 = "ã"|
-    # |071 = "Ã" |072 = "ô" |073 = "Ô" |074 = "é" |075 = "É" |076 = "á" |077 = "Á" |078 = "à" |079 = "À" |080 = "ê" |081 = "Ê" |082 = "Ç" |083 = "ç" |084 = "&"|
-    # |085 = "º" |086 = "|" |087 = "," |088 = "." |089 = ";" |090 = ":" |091 = "ó" |092 = "Ó" |093 = "0" |094 = "1" |095 = "2" |096 = "3" |097 = "4" |098 = "5"|
-    # |099 = "6" |100 = "7" |101 = "8" |102 = "9" |103 = "<" |104 = ">" |105 = "~" |106 = "$" |107 = "¨" |108 = "/" |109 = "\" |110 = """ |111 = "?" |
-
-    c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-         31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
-         43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
-         71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82,
-         83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107,
-         108, 109, 110, 111]
-    d = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
-         "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
-         "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " ", "-", "_", "+", "=", ")", "(", "*", "%", "#",
-         "@", "!", "'", "[", "]", "{", "}", "ã", "Ã", "ô", "Ô", "é", "É", "á", "Á", "à", "À", "ê",
-         "Ê", "Ç", "ç", "&", "º", "|", ",", ".", ";", ":", "ó", "Ó", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-         "<", ">", "~", "$", "¨", "/", "\\", '"', "?"]
-
-    return c, d
-
-
-##   Pega cada letra/simbolo (celula) escrito na msg_original e atribuí um novo valor com base na Tabela de Valores
-def converter_tabela(celula):
-    c, d = tabela()
-
-    if celula in d: return c[d.index(celula)]
-    return 0
-
-
-##   Tabela de Valores - Descriptografa os algarismos
-def decrypter(celula):
-    c, d = tabela()
-
-    if celula in c: return d[c.index(celula)]
-    return ""
-
-
-##   Processo de geração da chave de encriptação
-
-
-
-def GerarChave():
+def gerar_chave():
         
     #  Geração da Chave:
     key       = list(input("\n\nDigite uma Frase qualquer, quanto maior, mais segura será sua encriptação: \n\n"))
@@ -80,7 +109,7 @@ def GerarChave():
     chave_dec = [random.randint(0,999),random.randint(0,999),random.randint(0,999)]
 
     if key == []: key = list("Frase Padrão")      
-    for x in range(0,len(key)): key[x] = converter_tabela(key[x])
+    for x in range(0,len(key)): key[x] = dicio_encript[key[x]]
     
         
     #Algarismo 3---
@@ -119,120 +148,109 @@ def GerarChave():
     chave_dec.insert(3,"-")
     for x in chave_dec: chave_dec[chave_dec.index(x)] = str(x)
         
-    print("\nSua chave de Desencriptação: ",ListToText(chave_dec))
+    clean()
+    print("\nSua chave de Desencriptação: {}\n\n".format("".join(chave_dec)))
         
-    return chave_dec    
-
+    return chave_dec
+#-----------------------------------------------------------------------------------------------------------------
+# converte a msg original na criptografada
     
-##   Processo de Criptografar
-def Criptografar(msg):
+def criptografar(msg):
+    
+    msg      = list(msg)
+    n_celula = len(msg)
+    
+    # Gera um novo valor para célula
+    for n in range(0, len(msg)):
+        
+        msg[n]    = dicio_encript[msg[n]] + complexidade(n_celula)
+        msg[n]    = str(msg[n]) + "#"
+        n_celula -= 1
+        
+    return "".join(msg)
+        
+#-----------------------------------------------------------------------------------------------------------------
+# converte a msg criptografada na original novamente
+    
+def descriptografar(msg):
+    
+    msg      = msg.split("#")
+    msg.remove("")
+    n_celula = len(msg)
+    
+    
+    for n in range(0, len(msg)):
+        
+        msg[n]    = int(msg[n]) - complexidade(n_celula)
+        
+        if msg[n] in dicio_decript: msg[n] = dicio_decript[msg[n]]
+        else: msg[n] = "*"
+        
+        n_celula -= 1
+        
+    return "".join(msg)
 
-    # Variáveis
-    n_celula    = len(msg)
-    msg_crypted = []
+#-----------------------------------------------------------------------------------------------------------------
 
-    # Processamento
-    for x in msg:
-        valor_final = str(converter_tabela(x) + complexidade(n_celula))
-        msg_crypted.append(valor_final + separador(valor_final))
-        n_celula = n_celula - 1
+def menu():
 
-    ##   Separação da mensagem Criptografada em bytes (Processo Independente e meramente estético, se apagado, não alterará a estrutura do script)
+    while 1 > 0:
+        print('*******************************************')
+        print("1) Criptografar \n2) Descriptografar \n3) Configurações \n4) Cancelar")
+        print('*******************************************\n\n')
+        
+        opcao = escolha("", "Escolha uma Opção", "cls")
+        
+        if opcao not in ["1", "2", "3", "4"]: print("Opção Invalida!\n\n")
+        else: break
+    
+    if opcao != "4": processo(opcao)
 
-    # Váriáveis:
-    msg_bytes                   = []
-    qntd_algarismo              = 0
+#-----------------------------------------------------------------------------------------------------------------
 
-    # Processamento
-    for x in (list(ListToText(msg_crypted))):
+def processo(opcao):
+    
+    global chave_dec
+    
+    if opcao == "1":
+        
+        msg       = escolha("cls", "Digite uma mensagem a ser Criptografada: ", "cls")
+        chave_dec = gerar_chave()
+        msg       = criptografar(msg)
+    
+        print("Mensagem Criptografada gerada!! \n")
+        print("-------------------------------------------\n\n\n")
+        
+        
+        escrever_arquivo("cifra", msg)
+        
+        
+    if opcao == "2":
+        
+        msg = escolha("cls", "Digite a mensagem a ser Descriptografada: ", "cls")
+    
+        # Recepção da Chave
+        while len(chave_dec) != 3:
+            
+            chave_dec = list(input("\nDigite a chave de Desencriptação: \n\n"))
+            chave_dec = "".join(chave_dec)
+            chave_dec = chave_dec.split("-")
+            clean()
+        
+        msg = descriptografar(msg)
+        
+        print("\nMensagem Descriptografada: \n\n{} \n".format(msg))
+        print("-------------------------------------------\n\n\n")
+    
+    if confirmar("", "Deseja Continuar? ", "cls"): menu()
 
-        qntd_algarismo          = qntd_algarismo + 1
+#-----------------------------------------------------------------------------------------------------------------
   
-        if qntd_algarismo > 8:
-            qntd_algarismo      = 0
-            msg_bytes           = msg_bytes + [" "] + [x]   
-        else:     
-            msg_bytes           = msg_bytes + [x]
-            
-    msg_crypted = msg_bytes
+### *** Fim - Funções *** ###
 
-    return ListToText(msg_crypted)
+#################################################################################################################
 
-    ##  Fim!
+chave_dec = ""
 
-
-##   Processo de Desencriptação da mensagem
-def Descriptografar(msg):
-
-    ##  Separa os Algarismos nos valores originais.
-
-    # Váriáveis
-    celula_sep  = ""
-    msgCryp_sep = []
-
-    # Processo
-    for x in msg:
-
-        if x.isalpha():
-            msgCryp_sep.append(celula_sep)
-            celula_sep = ""
-
-        if x.isnumeric():
-            celula_sep = celula_sep + str(x)
-
-    ##   Processo de Desencriptação da mensagem
-
-    # Variáveis
-    qntd_celula = len(msgCryp_sep)
-    msg_descrypted = []
-
-    # Processamento
-    for x in msgCryp_sep:
-
-        if not x.isnumeric(): x = 0
-
-        valor_final = str(int(x) + descomplexar(qntd_celula))
-        msg_descrypted.append(decrypter(int(valor_final)))
-        qntd_celula = qntd_celula - 1
-
-    ##
-        
-    return ListToText(msg_descrypted)
-
-
-### FIM: ---- Funções ----
-
-##  Escolha do Processo
-op = input("Digite o numero relativo ao que deseja fazer: \n1 - Criptografar \n2 - Descriptografar \n\n")
-
-while op != "1" and op != "2":
-    print("\nOpção Invalida!! \n")
-    op = input("Digite novamente: ")
-
-
-## Gera a mensagem Criptografada
-if op == "1":
-    
-    msg = (input("\nDigite uma mensagem a ser Criptografada: \n\n"))
-
-    chave_dec = GerarChave()
-    
-    print("\nMensagem Criptografada: \n\n{} ".format(Criptografar(msg)))
-
-## Gera a mensagem Descriptografada
-elif op == "2":
-    
-    msg_c    = input("\nDigite uma mensagem a ser Desciptografada: \n\n")
-
-    # Impressão de Chave
-    chave_dec = list(input("\nDigite a chave de Desencriptação: \n\n"))
-    chave_dec = "".join(chave_dec)
-    chave_dec = chave_dec.split("-")
-            
-    while len(chave_dec) != 3: 
-        print("\n\nChave Invalida!\n")
-        chave_dec = list(input("\nDigite a chave de Desencriptação: \n\n"))
-        chave_dec = "".join(chave_dec)
-        chave_dec = chave_dec.split("-")
-    
-    print("\nMensagem Descriptografada: \n\n{} ".format( Descriptografar(msg_c)) )
+clean()
+menu ()
